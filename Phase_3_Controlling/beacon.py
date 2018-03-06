fe3c72a95203d7761d3711ab782548c8f4123f89
Matthew Du <crepuscian@foxmail.com>
2018-03-06 12:34:02 +0800

    Beacon controller

--- Phase_3_Controlling/beacon.py
+++ Phase_3_Controlling/beacon.py
@@ -3,57 +3,41 @@ import time
 from sys import exit
 
 BCN_LIGHT_PERIOD = 0.1
-BCN_LIGHT_PERIOD_LONG = 0.2
+BCN_LIGHT_PERIOD_LONG = 0.5
 BCN_STOP_INTERVAL = 2
-BCN_RUN_INTERVAL = 0.5
+BCN_RUN_INTERVAL = 1
 
 bcnStatus = 0
 
-class Beacon:
-    def __init__(self):
-        self.status = 0
-
-    def singleFlash():
-        self.status = 1
+def flashBeacon():
+    mode = bcnStatus
+    if mode == 0:
+        print("on")
         time.sleep(BCN_LIGHT_PERIOD)
-        self.status = 0
-
-    def doubleFlash():
-        for cycle in range(0,1):
-            self.status = 1
+        print("off")
+    elif mode == 1:
+        for cycle in range(2):
+            print("on")
             time.sleep(BCN_LIGHT_PERIOD)
-            self.status = 0
+            print("off")
             time.sleep(BCN_LIGHT_PERIOD)
-
-    def longFlash():
-        self.status = 1
+    elif mode == 2:
+        print("on")
         time.sleep(BCN_LIGHT_PERIOD_LONG)
-        self.status = 0
+        print("off")
 
-    def init():
-        global beaconThread
-        if bcnStatus == 0:
-            beaconThread = threading.Timer(BCN_STOP_INTERVAL, self.init)
-            beaconThread.start()
-        # elif bcnStatus == 1:
-        #     beaconThread = threading.Timer(BCN_STOP_INTERVAL, self.init)
-        #     beaconThread.start()
-        # elif bcnStatus == 2:
-        #     beaconThread = threading.Timer(BCN_RUN_INTERVAL, self.init)
-        #     beaconThread.start()
+def indicator_Write():
+    flashBeacon()
+    global beaconThread
+    if bcnStatus == 0:
+        beaconThread = threading.Timer(BCN_STOP_INTERVAL, indicator_Write)
+        beaconThread.start()
+    elif bcnStatus == 1:
+        beaconThread = threading.Timer(BCN_STOP_INTERVAL, indicator_Write)
+        beaconThread.start()
+    elif bcnStatus == 2:
+        beaconThread = threading.Timer(BCN_RUN_INTERVAL, indicator_Write)
+        beaconThread.start()
 
-        # if bcnStatus == 0:
-        #     self.singleFlash()
-        # elif bcnStatus == 1:
-        #     self.doubleFlash()
-        # elif bcnStatus == 2:
-        #     self.longFlash()
-
-flashlight = Beacon()
-
-beaconThread = threading.Timer(BCN_STOP_INTERVAL, flashlight.init)
+beaconThread = threading.Timer(BCN_STOP_INTERVAL, indicator_Write)
 beaconThread.start()
-
-while True:
-    print(flashlight.status)
-    time.sleep(0.02)
