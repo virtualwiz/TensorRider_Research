49f0bfbd4e50cd3ba36ae1fe54af7489811fd398
Matthew Du <crepuscian@foxmail.com>
2018-03-24 21:25:11 +0800

    Tfrecord decoding

--- Phase_6_Image_Preprocessing/tfrecord_check.py
+++ Phase_6_Image_Preprocessing/tfrecord_check.py
@@ -3,8 +3,11 @@ from skimage import io
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
-# import numpy
-# numpy.set_printoptions(threshold=numpy.nan)
+def rgb2gray(rgb):
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    # gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    gray = 0.5 * r + 0.25 * g + 0.25 * b
+    return gray
 
 record_iterator = tf.python_io.tf_record_iterator(path="record.tfrecords")
 
@@ -18,8 +21,9 @@ for string_record in record_iterator:
 
     image_1d = np.fromstring(imageString, dtype=np.uint8)
     image = image_1d.reshape((120, 160, 3))
+    image = rgb2gray(image)
 
-    # print(image)
+    print(image.shape)
 
     io.imshow(image)
     plt.show()
---
+++ Phase_7_Neural_Network_Testrun/decode.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import tensorflow as tf
+import tensorlayer as tl
+
+def read_and_decode(filename):
+    # generate a queue with a given file name
+    filename_queue = tf.train.string_input_producer([filename])
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)  # return the file and the name of file
+    features = tf.parse_single_example(
+        serialized_example,  # see parse_single_sequence_example for sequence example
+        features={
+            'label': tf.FixedLenFeature([], tf.int64),
+            'image': tf.FixedLenFeature([], tf.string),
+        })
+    # You can do more image distortion here for training data
+    img = tf.decode_raw(features['image'], tf.uint8)
+    img = tf.reshape(img, [120, 160, 3])
+    gimg = tf.image.rgb_to_grayscale(img)
+    label = tf.cast(features['label'], tf.int64)
+    return img, gimg, label
+
+img, gimg, label = read_and_decode("train.tfrecords")
+
+img_batch, label_batch = tf.train.shuffle_batch([img, label], batch_size=8, capacity=30, min_after_dequeue=10, num_threads=8)
+print("img_batch   : %s" % img_batch.shape)
+print("label_batch : %s" % label_batch.shape)
+
+with tf.Session() as sess:
+    tl.layers.initialize_global_variables(sess)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    for i in range(500):  # number of mini-batch (step)
+        print("Step %d" % i)
+        val, l = sess.run([img_batch, label_batch])
+        print(val.shape, l)
+        tl.visualize.images2d(val, second=1, saveable=False, name='batch', dtype=None, fig_idx=2020121)
+
+    coord.request_stop()
+    coord.join(threads)
+    sess.close()
